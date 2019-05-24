0acd3c4163fe23d0c603af12423233b8878f49e4
Iseeumhmm <rickheffren@Ricks-MacBook-Pro.local>
2019-05-24 09:56:19 -0400

    fixed navbar collapse button

--- wp-content/themes/berean-baptist-church/functions.php
+++ wp-content/themes/berean-baptist-church/functions.php
@@ -122,14 +122,6 @@ add_action( 'widgets_init', 'berean_baptist_church_widgets_init' );
 wp_enqueue_style( 'wpb-google-fonts', 'https://fonts.googleapis.com/css?family=EB+Garamond:400,700|Work+Sans:300,400,700', false ); 
 wp_enqueue_style( 'FontAwesome', get_template_directory_uri() . '/font-awesome/css/all.css', false );
 
-
-wp_register_script( 'jQuery', 'https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo', null, null, true );
-wp_enqueue_script('jQuery');
-wp_register_script( 'PopperJs', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js', null, null, true );
-wp_enqueue_script('PopperJs');
-wp_register_script( 'Bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js', null, null, true );
-wp_enqueue_script('Bootstrap');
-
 function my_theme_scripts_function() {
 	wp_enqueue_script( 'myscript', get_template_directory_uri() . '/js/script.js');
 }
--- wp-content/themes/berean-baptist-church/header.php
+++ wp-content/themes/berean-baptist-church/header.php
@@ -15,6 +15,7 @@
 <html class="mt-0" <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="profile" href="https://gmpg.org/xfn/11">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -31,7 +32,7 @@
 					<div class="col-md-12 col-lg-11 col-xl-10">
 						<nav class="navbar navbar-expand-lg navbar-light">
 							<div class="logo">
-								<a href="/<?php echo get_site_url() ?>/home" class="navbar-brand"><h1>Berean Baptist Church</h1></a>
+								<a href="<?php echo get_site_url() ?>/" class="navbar-brand"><h1>Berean Baptist Church</h1></a>
 								<p>London, Ontario</p>
 							</div>
 							<button class="navbar-toggler mr-md-7" data-toggle="collapse" data-target="#navbarMenu">
